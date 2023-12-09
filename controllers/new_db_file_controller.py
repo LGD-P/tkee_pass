2dcd7a8790445f3a78b9bf400f7ead4ec6c4d3da
LGD-P <nplgdjp@gmx.Com>
2023-12-09 19:45:58 +0100

    pass strength entry calculate & updated

--- controllers/new_db_file_controller.py
+++ controllers/new_db_file_controller.py
@@ -1,5 +1,6 @@
 from tkinter import filedialog, Toplevel
 import ttkbootstrap as ttk
+from zxcvbn import zxcvbn
 from pykeepass import PyKeePass
 
 
@@ -9,6 +10,8 @@ class CreateFile():
         self.root = Toplevel(parent)
         self.root.title('Create Database')
         self.geometry = self.root.geometry("650x300")
+
+        # FILE NAME FRAM TO COME
 
         # FILE PATH FRAME
         self.filepath_frame = ttk.Frame(self.root)
@@ -25,6 +28,7 @@ class CreateFile():
         self.password_frame = ttk.Frame(self.root)
         self.password_label = ttk.Label(self.password_frame, text="Enter Password", width=13)
         self.password_entry = ttk.Entry(self.password_frame, style='danger', show='*', width=40)
+        self.password_entry.bind("<Key>", self.get_password_strength) # to update each entry
         self.password_button = ttk.Button(self.password_frame, style="danger-outline", text="Ok",
                                           command=self.password_button, width=9)
         self.password_label.grid(row=0, column=0, padx=15, sticky="W")
@@ -32,13 +36,28 @@ class CreateFile():
         self.password_button.grid(row=0, column=2, padx=15, sticky="W")
         self.password_frame.place(y=150)
 
-        # ERROR MESSAGE FRAME
-        self.wrong_password_frame = ttk.Frame(self.root)
-        self.wrong_password_label = ttk.Label(self.wrong_password_frame, justify='center', anchor='center',
-                                              foreground="red", width=73)
-        self.wrong_password_label.grid(row=0, column=1, padx=15)
-        self.wrong_password_frame.place(y=200)
+        # PASSWORD STRENGTH MESSAGE FRAME
+        self.password_strength_frame = ttk.Frame(self.root)
+        self.very_week_label = ttk.Label(self.password_strength_frame, justify='center', anchor='center',
+                                         background="red", text='very week', width=18)
+        self.week_label = ttk.Label(self.password_strength_frame, justify='center', anchor='center',
+                                    background="red", text='week', width=18)
+        self.ok_label = ttk.Label(self.password_strength_frame, justify='center', anchor='center',
+                                  background="orange", text='ok', width=18)
+        self.good_label = ttk.Label(self.password_strength_frame, justify='center', anchor='center',
+                                    background="green", text='good', width=18)
+        self.very_week_label.grid(row=0, column=0, padx=5, sticky="W")
+        self.week_label.grid(row=0, column=1, padx=5, sticky="W")
+        self.ok_label.grid(row=0, column=2, padx=5, sticky="W")
+        self.good_label.grid(row=0, column=3, padx=5, sticky="W")
 
+        self.password_strength_frame.place(y=200)
+
+    def get_password_strength(self, *args, **kwargs):
+        password = self.password_entry.get()
+        strength = zxcvbn(password)['score']
+        print(strength)
+        return strength
 
     def filepath_button(self):
         """Ask user to choose a directory to register file"""
@@ -49,8 +68,10 @@ class CreateFile():
 
     def password_button(self):
         """Register and check strength"""
-        pass
+        password = self.password_entry.get()
+        print(password)
+        #
+        return password
 
     def run(self):
         self.root.mainloop()
-

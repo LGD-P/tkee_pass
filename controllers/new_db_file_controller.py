67f46100affaab615dfa75d8beddf32361fb6b9e
LGD-P <nplgdjp@gmx.Com>
2024-01-26 18:19:50 +0100

    file creation & check param file before creation

--- controllers/new_db_file_controller.py
+++ controllers/new_db_file_controller.py
@@ -1,9 +1,11 @@
+import os
 from tkinter import filedialog, Toplevel
 import ttkbootstrap as ttk
 from zxcvbn import zxcvbn
-from pykeepass import PyKeePass
+from pykeepass import PyKeePass, create_database
 import re
 from threading import Timer
+
 
 class CreateFile():
     def __init__(self, parent):
@@ -13,11 +15,11 @@ class CreateFile():
         self.geometry = self.root.geometry("650x300")
 
         # FILE NAME ERROR FRAME
-        self.error_filename_frame = ttk.Frame(self.root)
-        self.error_filename_label = ttk.Label(self.error_filename_frame, justify='center', anchor='center',
-                                              width=75)
-        self.error_filename_label.grid(row=0, column=0, columnspan=2, padx=15, pady=5, sticky="W")
-        self.error_filename_frame.place(y=50)
+        self.message_frame = ttk.Frame(self.root)
+        self.message_frame_label = ttk.Label(self.message_frame, justify='center', anchor='center',
+                                             width=75)
+        self.message_frame_label.grid(row=0, column=0, columnspan=2, padx=15, pady=5, sticky="W")
+        self.message_frame.place(y=50)
 
         # FILE NAME FRAME
         self.filename_frame = ttk.Frame(self.root)
@@ -38,12 +40,11 @@ class CreateFile():
         self.filepath_button.grid(row=0, column=2, padx=15, sticky="W")
         self.filepath_frame.place(y=150)
 
-
         # PASSWORD FRAME
         self.password_frame = ttk.Frame(self.root)
         self.password_label = ttk.Label(self.password_frame, text="Enter Password", width=13)
         self.password_entry = ttk.Entry(self.password_frame, style='danger', show="*", width=40)
-        self.password_entry.bind("<KeyRelease>", self.get_password_strength) # to update each entry
+        self.password_entry.bind("<KeyRelease>", self.get_password_strength)  # to update each entry
         self.password_button = ttk.Button(self.password_frame, style="danger-outline", text="Ok",
                                           command=self.password_button, width=9)
         self.password_label.grid(row=0, column=0, padx=15, sticky="W")
@@ -54,7 +55,7 @@ class CreateFile():
         # PASSWORD STRENGTH MESSAGE FRAME
         self.password_strength_frame = ttk.Frame(self.root)
         self.very_weak_label = ttk.Label(self.password_strength_frame, justify='center', anchor='center',
-                                        background="red",  width=18)
+                                         background="red", width=18)
         self.weak_label = ttk.Label(self.password_strength_frame, justify='center', anchor='center',
                                     background="red", width=18)
         self.ok_label = ttk.Label(self.password_strength_frame, justify='center', anchor='center',
@@ -112,24 +113,50 @@ class CreateFile():
         self.filepath_entry.insert(0, path)
         return path
 
-    def is_valid_filename(self,filename):
+    def is_valid_filename(self, filename):
         pattern = r'^[a-zA-Z0-9_\-\.]+$'
         return re.match(pattern, filename) is not None
 
-    def hide_error_message(self):
-        return self.error_filename_label.configure(text="", background="black")
+    def hide_message(self):
+        return self.message_frame_label.configure(text="", background="black")
+
+    def is_file_ready_to_save(self, filename, path, password):
+        """ Check that every parameter for file creation is completed, else return error message & False
+        """
+        if self.is_valid_filename(filename) is False or filename == "":
+            self.message_frame_label.configure(text="File name needed - No Space & No Special characters "
+                                                    "allowed in your filename", foreground="red")
+            Timer(5.0, self.hide_message).start()
+            return False
+
+        elif path == "":
+            self.message_frame_label.configure(text="Please select a path to your file",
+                                               foreground="red")
+            Timer(5.0, self.hide_message).start()
+            return False
+
+        elif password == "":
+            self.message_frame_label.configure(text="Please enter a strong password - prefer it strong",
+                                               foreground="red")
+            Timer(5.0, self.hide_message).start()
+            return False
+        else:
+            return True
 
     def password_button(self):
-        """Register and check strength"""
+        """Create kdbx file"""
         password = self.password_entry.get()
         filename = self.filename_entry.get()
-        if self.is_valid_filename(filename) is False:
-            self.error_filename_label.configure(text="Special characters are not allowed in your filename",
-                                                foreground="red")
-            Timer(3.0, self.hide_error_message).start()
-        else:
-            print("File name = ", filename)
-        return password
+        path = self.filepath_entry.get()
+
+        filename_with_extension = f"{filename}.kdbx"
+        file_path = os.path.join(path, filename_with_extension)
+
+        if self.is_file_ready_to_save(filename, path, password) is True:
+            file = create_database(file_path, password=password, keyfile=None, transformed_key=None)
+            file.save()
+            self.message_frame_label.configure(text="File created", foreground="green")
+            Timer(5.0, self.hide_message).start()
 
     def run(self):
         self.root.mainloop()

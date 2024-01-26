8d8e790471058e2c4735111a281713322981be2b
LGD-P <nplgdjp@gmx.Com>
2024-01-26 17:00:09 +0100

    add filename frame & 3sec error message

--- controllers/new_db_file_controller.py
+++ controllers/new_db_file_controller.py
@@ -2,16 +2,30 @@ from tkinter import filedialog, Toplevel
 import ttkbootstrap as ttk
 from zxcvbn import zxcvbn
 from pykeepass import PyKeePass
-
+import re
+from threading import Timer
 
 class CreateFile():
     def __init__(self, parent):
-        # Toplevel to be able to open new window with keeping style parameters
+        # Toplevel to be able to open new window keeping style parameters
         self.root = Toplevel(parent)
         self.root.title('Create Database')
         self.geometry = self.root.geometry("650x300")
 
-        # FILE NAME FRAME TO COME
+        # FILE NAME ERROR FRAME
+        self.error_filename_frame = ttk.Frame(self.root)
+        self.error_filename_label = ttk.Label(self.error_filename_frame, justify='center', anchor='center',
+                                              width=75)
+        self.error_filename_label.grid(row=0, column=0, columnspan=2, padx=15, pady=5, sticky="W")
+        self.error_filename_frame.place(y=50)
+
+        # FILE NAME FRAME
+        self.filename_frame = ttk.Frame(self.root)
+        self.filename_label = ttk.Label(self.filename_frame, text="File name", width=13)
+        self.filename_entry = ttk.Entry(self.filename_frame, style='danger', width=40)
+        self.filename_label.grid(row=0, column=0, padx=15, sticky="W")
+        self.filename_entry.grid(row=0, column=1, padx=15, sticky="W")
+        self.filename_frame.place(y=100)
 
         # FILE PATH FRAME
         self.filepath_frame = ttk.Frame(self.root)
@@ -22,20 +36,20 @@ class CreateFile():
         self.filepath_label.grid(row=0, column=0, padx=15, sticky="W")
         self.filepath_entry.grid(row=0, column=1, padx=15, sticky="W")
         self.filepath_button.grid(row=0, column=2, padx=15, sticky="W")
-        self.filepath_frame.place(y=100)
+        self.filepath_frame.place(y=150)
 
 
         # PASSWORD FRAME
         self.password_frame = ttk.Frame(self.root)
         self.password_label = ttk.Label(self.password_frame, text="Enter Password", width=13)
-        self.password_entry = ttk.Entry(self.password_frame, style='danger', show='*', width=40)
+        self.password_entry = ttk.Entry(self.password_frame, style='danger', show="*", width=40)
         self.password_entry.bind("<KeyRelease>", self.get_password_strength) # to update each entry
         self.password_button = ttk.Button(self.password_frame, style="danger-outline", text="Ok",
                                           command=self.password_button, width=9)
         self.password_label.grid(row=0, column=0, padx=15, sticky="W")
         self.password_entry.grid(row=0, column=1, padx=15, sticky="W")
         self.password_button.grid(row=0, column=2, padx=15, sticky="W")
-        self.password_frame.place(y=150)
+        self.password_frame.place(y=200)
 
         # PASSWORD STRENGTH MESSAGE FRAME
         self.password_strength_frame = ttk.Frame(self.root)
@@ -47,7 +61,7 @@ class CreateFile():
                                   background="orange", width=18)
         self.good_label = ttk.Label(self.password_strength_frame, justify='center', anchor='center',
                                     background="green", width=18)
-        self.password_strength_frame.place(y=200)
+        self.password_strength_frame.place(y=250)
 
     def get_password_strength(self, *args, **kwargs):
         """Display strength of the input password"""
@@ -98,11 +112,23 @@ class CreateFile():
         self.filepath_entry.insert(0, path)
         return path
 
+    def is_valid_filename(self,filename):
+        pattern = r'^[a-zA-Z0-9_\-\.]+$'
+        return re.match(pattern, filename) is not None
+
+    def hide_error_message(self):
+        return self.error_filename_label.configure(text="", background="black")
+
     def password_button(self):
         """Register and check strength"""
         password = self.password_entry.get()
-        print(password)
-        #
+        filename = self.filename_entry.get()
+        if self.is_valid_filename(filename) is False:
+            self.error_filename_label.configure(text="Special characters are not allowed in your filename",
+                                                foreground="red")
+            Timer(3.0, self.hide_error_message).start()
+        else:
+            print("File name = ", filename)
         return password
 
     def run(self):
